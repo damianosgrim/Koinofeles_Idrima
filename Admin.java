ce59380de3949cc1fce432c55ae18da03c3dc95a
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-04-29 14:40:51 +0300

    Delete Admin.java

--- Admin.java
+++
@@ -1,8 +0,0 @@
-public class Admin extends User {
-private boolean isAdmin=true;
-
-public Admin (String firstname, String lastname, String phone) 
-{ super(firstname, lastname, phone); }
-
-public boolean isAdmin() { return true; }
-}

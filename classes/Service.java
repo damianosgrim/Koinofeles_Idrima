3ce4a7c1ef6800af29b5481cc2be2cb01959b68e
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-09 16:31:43 +0300

    Update Service.java

--- classes/Service.java
+++ classes/Service.java
@@ -2,4 +2,8 @@ public class Service extends Entity{
       public String getDetails(){
           return ("the object is service");
         }
+      //@override
+    public String toString(){
+    return "info: " + getEntityInfo() + "details "+ getDetails();
+    }
 }

3f8e961b6ad7b68dc4924996d4a11448b67106f8
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-09 16:30:17 +0300

    Update Entity.java

--- classes/Entity.java
+++ classes/Entity.java
@@ -3,6 +3,15 @@ public abstract class Entity{
     private String description; //συντομη περγραφή
     private int id; //κωδικός είδους
     
+    public String getName(){return name;}
+    public void setName(String name){this.name=name;}
+    
+     public String getDescription(){return description;}
+     public void setDescription(String description){this.description=description;}
+    
+    public int getId(){return id;}
+    public void setId(int id){this.id=id;}
+     
     public String getEntityInfo(){
         return name + description + id;
     }
@@ -10,8 +19,6 @@ public abstract class Entity{
     abstract String getDetails();
     
     public String toString(){
-        this.getEntityInfo();
-        this.getDetails();
-        return this.getEntityInfo() + this.getDetails();
+        return "info: " + getEntityInfo() + "details "+ getDetails();
         }
 }

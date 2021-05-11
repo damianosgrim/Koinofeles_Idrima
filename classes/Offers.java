a1722284f6dcef1ca23af14aba154f7dd3f3273c
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-11 14:32:13 +0300

    Update Offers.java

--- classes/Offers.java
+++ classes/Offers.java
@@ -1,3 +1,6 @@
 public class Offers extends RequestDonationList{
-
+    public void commit(){  
+        
+    rdEntities.remove(); //remove an object from rdEntities
+    } //update currentDonations with rdEntities.
 }

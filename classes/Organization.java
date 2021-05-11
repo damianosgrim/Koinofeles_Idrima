cf493e497265696110937ad28756e8a4378b18fc
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-05-11 19:45:26 +0300

    Update Organization.java

--- classes/Organization.java
+++ classes/Organization.java
@@ -3,12 +3,16 @@ public class Organization {
 private String name;
 private Admin admin;
 
+public Organization(String name, Admin admin) 
+{this.name=name;
+ this.admin=admin; }
+
 ArrayList<Entity> entityList = new ArrayList<Entity>();
 ArrayList<Donator> donatorList = new ArrayList<Donator>();
 ArrayList<Beneficiary> beneficiaryList = new ArrayList<Beneficiary>();
 
 private RequestDonationList currentDonations;
-
+  
 public Admin getAdmin() {return admin;}
 public void setAdmin(Admin admin) {this.admin=admin;}
 
@@ -25,4 +29,10 @@ public void listEntities() {for (Entity entity : entityList) { System.out.printl
 public void listDonators() {for (Donator donator : donatorList) { System.out.println(donator.getFirstName() + donator.getLastName() ); }}
 public void listBeneficiary() {for (Beneficiary beneficiary : beneficiaryList) { System.out.println(beneficiary.getFirstName() + beneficiary.getLastName() ); }}
 
+public int exists(String num){   
+boolean num1 = donatorList.contains(num); 
+boolean num2 = beneficiaryList.contains(num);
+if(num1||num2)
+{return 0;} else {return 1;}
+}
 }

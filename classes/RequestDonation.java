68214b1be04717c8fa4cfb3930746f8706dcd07a
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-11 14:31:12 +0300

    Update RequestDonation.java

--- classes/RequestDonation.java
+++ classes/RequestDonation.java
@@ -1,6 +1,18 @@
+import java.util.*;
 public class RequestDonation
 {
     Entity entity;
     double quantity; 
-    //interface comparator??!!
+    
+    public RequestDonation(Entity entity, double quantity){
+    this.entity=entity;
+    this.quantity=quantity;
+    }
+    //interface comparator
+    public int compare(RequestDonation d1, RequestDonation d2)
+        {
+            if (d1.id1==d2.id2){return 0;} //pws kalw to id apo to entity
+            else if(d1.id=!d2.id){return 1;}
+        }
+    }
 }

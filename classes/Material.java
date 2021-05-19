960c92868b872545eb9c7e2aa6a80d66ec2ddd7d
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-19 21:53:37 +0300

    Update Material.java

--- classes/Material.java
+++ classes/Material.java
@@ -1,8 +1,8 @@
 public class Material extends Entity{
 
     private double level1; //η ποσοτητα που δικαιουται 1 ατομο
-    private double level2; // η ποσοτητα που δικαιουνται 2-4 ατομα
-    private double level3; // η ποσότητα που δικαιουνται 5 και περισσότερα ατομα
+    private double level2; //η ποσοτητα που δικαιουνται 2-4 ατομα
+    private double level3; //η ποσότητα που δικαιουνται 5 και περισσότερα ατομα
     
     public String getDetails(){
         return (level1 + level2 + level3 + " the object is material" );
@@ -11,7 +11,7 @@ public class Material extends Entity{
     public String toString(){
         return "info: " + getEntityInfo() + "details "+ getDetails();
      }
-
-    
+     
+    //prepei na epistrefei ta levels, na doume ti kanoun levels 
 }
         

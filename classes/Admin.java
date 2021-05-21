23964fcfa8bb036ef2004da62dd667630034a8e5
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-05-21 14:40:55 +0300

    Delete Admin.java

--- classes/Admin.java
+++
@@ -1,7 +0,0 @@
-public class Admin extends User {
-
-public Admin (String firstname, String lastname, String phone) 
-{ super(firstname, lastname, phone); }
-
-public boolean isAdmin() { return true; }//Override
-}

80df4f4f9b4ab34dbc63c923b8fad1dc55dddff6
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-05-21 14:42:19 +0300

    Delete Offers.java

--- classes/Offers.java
+++
@@ -1,6 +0,0 @@
-public class Offers extends RequestDonationList{
-    public void commit(){  
-        ArrayList <rdEntities> copy = RequestDonationList (currentDonations); //update currentDonation with copy from rdEntities
-    rdEntities.clear(); //clear rdEntities
-    } //update currentDonations with rdEntities.
-}

c0407c0205d00dcfd0261816afed3feda465271d
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-05-21 14:41:29 +0300

    Delete Donator.java

--- classes/Donator.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-public class Donator extends User {
-ArrayList<Offers> offersList = new ArrayList<Offers>();
-
-
-// na koitaksoume wrappers
-public Donator(String firstname, String lastname, String phone) 
-{ super(firstname, lastname, phone); }
-}

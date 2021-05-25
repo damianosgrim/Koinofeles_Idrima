c8fa0f5f4e2e0bd59d82979e3c6a8ddf2f67e1eb
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-25 11:12:24 +0300

    Update RequestDonationList.java

--- Classes/RequestDonationList.java
+++ Classes/RequestDonationList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class RequestDonationList{
-    public static ArrayList<RequestDonation> rdEntities=new ArrayList<RequestDonation>(); //δημιουργία λίστας
+    public static ArrayList<RequestDonation> rdEntities=new ArrayList<RequestDonation>(); //create a list
     static RequestDonation rdon;
     
     public static ArrayList<RequestDonation> getrdEntities(){
@@ -10,11 +10,11 @@ public class RequestDonationList{
     //constructor χωρίς ορίσματα
     public RequestDonationList(){}
     
-    //μέθοδοι
+    //methods
     public static RequestDonation getRequestDonation(){return rdon;}
     
     
-    //καταχώρηση ενός id και επιστροφή του κατάλληλου rdEntities
+    // give id of entity and return requestdonation 
     public static RequestDonation get(int id){
         RequestDonation r =null;
         for (int i = 0; i<rdEntities.size(); i++){
@@ -23,7 +23,7 @@ public class RequestDonationList{
          return r;
       }
     
-    //προσθήκη ενός requestdonation στην rdentities και αλλαγή της ποσότητας εάν υπάρχει ήδη   
+    //add a requestdonation in rdentities and if already exists change  quantity    
     public static void add(RequestDonation requestdonation, double quant){  // εξαιρεση!!!
          for (int i = 0; i<rdEntities.size(); i++)
         {
@@ -34,3 +34,30 @@ public class RequestDonationList{
         else if (rdEntities.get(i).getEntity().getId() != requestdonation.getEntity().getId()){
         rdEntities.add(requestdonation);}
     }  
+    } 
+
+    //remove a requestdonation from the list
+     public void remove(RequestDonation requestdonation){
+        rdEntities.remove(requestdonation);  
+    }
+    
+    
+    public void modify(RequestDonation d ,double quantity){
+        if(rdEntities.contains(d)){
+        d.setQuantity(quantity);
+        rdEntities.get(rdEntities.indexOf(d)).setQuantity(quantity);
+    }
+    }//change quantity of a product
+    
+    public void monitor(){
+        for (RequestDonation requestdon : rdEntities) { 
+            System.out.println("name of donation " + requestdon.getEntity().getName() + ", quantity " +requestdon.getQuantity()); }
+        }//print list's content
+    
+    public void reset(){
+    rdEntities.clear();
+    }//clear the list 
+    
+}
+    
+    

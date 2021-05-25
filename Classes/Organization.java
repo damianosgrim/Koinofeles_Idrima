136100918bc80524854d9c78fd2194b6272c82ee
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-25 14:13:23 +0300

    Update Organization.java

--- Classes/Organization.java
+++ Classes/Organization.java
@@ -6,14 +6,24 @@ static ArrayList<Entity> entityList = new ArrayList<Entity>();
 static ArrayList<Donator> donatorList = new ArrayList<Donator>();
 static ArrayList<Beneficiary> beneficiaryList = new ArrayList<Beneficiary>();
 
+    public static ArrayList<Donator> getdonatorList(){
+        return donatorList;
+    }
+    
+    public static ArrayList<Beneficiary> getbeneficiaryList(){
+        return beneficiaryList;
+    }
+    
+     public Organization(){}
     //Constructor για το Organization
     public Organization(String name, Admin admin) 
     {this.name=name;
      this.admin=admin; }
      
-    public static RequestDonationList currentDonations; //pithani prosthiki methodou add apo tin RDL ston setter
-    public RequestDonationList getCurrentDon(){return currentDonations;}
-    public void setCurrentDon(RequestDonationList currentDonations){this.currentDonations=currentDonations;}
+     public static ArrayList<RequestDonationList> currentDonations = new ArrayList<RequestDonationList>();
+   /* public static RequestDonationList currentDonations; //pithani prosthiki methodou add apo tin RDL ston setter
+    public static RequestDonationList getCurrentDon(){return currentDonations;}
+    public static void setCurrentDon(RequestDonationList currDon) {currentDonations=currDon;}*/
       
     public Admin getAdmin() {return admin;}
     public void setAdmin(Admin admin) {this.admin=admin;}
@@ -31,12 +41,6 @@ static ArrayList<Beneficiary> beneficiaryList = new ArrayList<Beneficiary>();
     public void listDonators() {for (Donator donator : donatorList) { System.out.println(donator.getFirstName() + donator.getLastName() ); }}
     public void listBeneficiary() {for (Beneficiary beneficiary : beneficiaryList) { System.out.println(beneficiary.getFirstName() + beneficiary.getLastName() ); }}
     
-    public static int exists(String num){   
-    boolean num1 = donatorList.contains(num); 
-    boolean num2 = beneficiaryList.contains(num);
-    if(num1||num2)
-    {return 0;} else {return 1;}
-    //να κοιτάξουμε wrappers
-    }
+    
 }
     

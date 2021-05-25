4c6b2f8ccbd1f6151c725dd3bda46a59dc7c5e03
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-25 14:12:21 +0300

    Update Menu.java

--- Classes/Menu.java
+++ Classes/Menu.java
@@ -1,27 +1,19 @@
 import java.util.*;
+import java.io.*;
 public class Menu {
-public static void Scan() {
-    Scanner scan = new Scanner(System.in);
+     static Scanner scan = new Scanner(System.in);
+     private static String ans;
+public static void Scan() throws NullLineReadedException{
+    System.out.println("Welcome to our Donator-Beneficiary system");
     System.out.println("Please enter a phone number: ");
-    String phone=scan.nextLine();
-    
-    /* boolean num1 = donatorList.contains(phone);
-    boolean num2 = beneficiaryList.contains(phone); */
-    
-    if(exists(phone)==0) { System.out.println("The phone number " + phone + " has been already added in the system"); }
-    else { System.out.println("Do you want to sign up in our system? (y/n): ");
-           String ans1=scan.nextLine();
-           if(ans1=="y"||ans1=="yes") {
-               System.out.println("Sign up as Beneficiary or Donator? (b/d): ");
-               String ans2=scan.nextLine();
-               if(ans2=="b"||ans2=="beneficiary") { System.out.println("You sign up as beneficiary"); }
-               else if(ans2=="d"||ans2=="donator") {System.out.println("You sign up as donator"); }
-            }
-           else if(ans1=="n"||ans1=="no") {System.out.println("Goodbyeeee :( ");}
+    boolean search = false;
+     ans=scan.nextLine();
+     for(int i = 0; i<Organization.getdonatorList().size(); i++){
+         if(Organization.getdonatorList().get(i).getPhone().equals(ans)){
+              System.out.println("The phone number " + phone + " has been already added in the system");
+         
         }
+   
+   
 } 
 }
-
-    
-   
-   

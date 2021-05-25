cfd2f337ad322ae405c2d875d6f7c485cb2f2fdc
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-25 14:14:50 +0300

    Update Request.java

--- Classes/Request.java
+++ Classes/Request.java
@@ -1,20 +1,49 @@
 public class Request extends RequestDonationList{
-    public void add(){
     
+    //@override
+    public static void add(RequestDonation requestdonation, double quant){  // εξαιρεση!!!
+         for (int i = 0; i<rdEntities.size(); i++)
+        {
+        if(rdEntities.get(i).getEntity().getId()== requestdonation.getEntity().getId()) {
+            requestdonation.setQuantity(requestdonation.getQuantity()+quant);
+            rdEntities.get(i).setQuantity(requestdonation.getQuantity()+quant);
+        }
+        else if (rdEntities.get(i).getEntity().getId() != requestdonation.getEntity().getId()){
+        rdEntities.add(requestdonation);}
+    } 
+   }
+    
+    public void modify(RequestDonation d ,double quantity){
+        if(rdEntities.contains(d)){
+        d.setQuantity(quantity);
+        rdEntities.get(rdEntities.indexOf(d)).setQuantity(quantity);
+    }
     }
     
-    public void modify(){
+    public boolean validRequestDonation(Beneficiary b, RequestDonation d){  // ελεγχος ποσοτητας
+        boolean l=false;
+       if(b.getNoPersons()==1) {
+           if(d.getQuantity()<=Material.getLevel1()){
+               l = true;
+        }
+       }
+        else if(b.getNoPersons()>=2 && b.getNoPersons()<=4) {
+             if(d.getQuantity()<=Material.getLevel2()){
+               l = true;
+        }
+        }
+        else if(b.getNoPersons()>=5) {
+             if(d.getQuantity()<=Material.getLevel3()){
+               l = true;
+        } 
+    }
+        return l;
+        
     }
     
-    public void validRequestDonation(Beneficiary b){
-        double level;
-       if(b.getNoPersons()==1) {level = 1;}
-        else if(b.getNoPersons()>=2 && b.getNoPersons()<=4) {level = 2;}
-        else if(b.getNoPersons()>=5) {level = 3;} 
-    }
         
         
-        
+}  
     
-}
+
 

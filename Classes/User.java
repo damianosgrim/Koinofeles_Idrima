6b1ab6e05d47b72de80a55fb299336087b72d787
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2021-05-26 15:52:53 +0300

    Update User.java

--- Classes/User.java
+++ Classes/User.java
@@ -1,6 +1,6 @@
 public abstract class User {
-private static String firstname;  //Επιλέγουμε να έχουμε και όνομα και επώνυμο αντί για σκέτο name
-private static String lastname;
+private  String firstname;  //Επιλέγουμε να έχουμε και όνομα και επώνυμο αντί για σκέτο name
+private  String lastname;
 private String phone;
 
 //constructor χωρίς ορίσματα
@@ -11,11 +11,11 @@ public User (String first, String last, String ph)
     lastname = last;
     phone = ph; }
 
-public static String getFirstName() { return firstname; }
-public static void setFirstName(String first) {firstname=first;}
+public  String getFirstName() { return firstname; }
+public  void setFirstName(String first) {firstname=first;}
 
-public static String getLastName() { return lastname; }
-public static void setLastName(String last) { lastname=last;}
+public  String getLastName() { return lastname; }
+public  void setLastName(String last) { lastname=last;}
 
 public String getPhone() { return phone; }
 public void setPhone(String phone) { this.phone=phone;}
@@ -23,4 +23,3 @@ public void setPhone(String phone) { this.phone=phone;}
 //Επιστρέφει false και για όλες τις υποκλάσεις εκτός απο το Admin που έχουμε κάνει override.
 public boolean isAdmin() { return false; } 
 }
- 

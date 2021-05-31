4d7751cdd4834b8eba259bf67fdcfa1f5a513e37
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:22:48 +0300

    Update Service.java

--- Classes/Service.java
+++ Classes/Service.java
@@ -2,18 +2,18 @@ import java.util.*;
 public class Service extends Entity{
     int EntityCategory=2;
     
+    //μέθοδος που επιστρέφει το είδος της δωρεάς
     public String getDetails(){
           return ("the object is service");
     }
         
-      //@override
+    //@override
     public String toString(){
     return "info: " + getEntityInfo() + "details "+ getDetails();
     }
     
-     //μέθοδος που δηλώνεται για να χρησιμοποιηθεί στην main ώστε να εκτυπώνει στον Admin την ποσότητα των Services
+    //μέθοδος που δηλώνεται για να χρησιμοποιηθεί στην main ώστε να εκτυπώνει στον Admin την ποσότητα των Services
     public int getEntityCategory(){
         return EntityCategory;
-    }
-      
+    }   
 }

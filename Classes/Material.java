038de14581223a29f9a3fa439d7f29ab5100c8dd
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:18:23 +0300

    Update Material.java

--- Classes/Material.java
+++ Classes/Material.java
@@ -6,31 +6,30 @@ public class Material extends Entity{
     private static double level3=20; //η ποσοτητα που δικαιουνται 5+ ατομα 
     public int EntityCategory=1;
     
+    //getters για levels
     public static double  getLevel1(){return level1;}
-     
     public static double getLevel2(){return level2;}
+    public static double getLevel3(){return level3;}
      
-     public static double getLevel3(){return level3;}
-     
-     public void setLevel1(double level1){this.level1=level1;}
-     
-     public void setLevel2(double level2){this.level2=level2;}
-     
-     public void setLevel3(double level3){this.level3=level3;}
+    ////setters για levels
+    public void setLevel1(double level1){this.level1=level1;}
+    public void setLevel2(double level2){this.level2=level2;}
+    public void setLevel3(double level3){this.level3=level3;}
     
+    //getters για επιστροφή των ποσοτήτων του κάθε level και του είδους 
     public  String getDetails(){
        
         return ("level1: "+ level1 + " level2 " + level2 + " level3 " + level3 + " this object is material");
         
     }
-     //@override    
+    
+    //μέθοδος για την εκτύπωση πληροφοριών και λεπτομεριών //@override    
     public String toString(){
         return "info: " + getEntityInfo() + "details "+ getDetails();
      }
     
-     //μέθοδος που δηλώνεται για να χρησιμοποιηθεί στην main ώστε να εκτυπώνει στον Admin την ποσότητα των Materials
+    //μέθοδος που δηλώνεται για να χρησιμοποιηθεί στην main ώστε να εκτυπώνει στον Admin την ποσότητα των Materials
     public int getEntityCategory(){
         return EntityCategory;
     }
-    //prepei na epistrefei ta levels, na doume ti kanoun levels 
 }

36ff980f165f8490c7f5410908e1f885db688f7c
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:16:03 +0300

    Update Admin.java

--- Classes/Admin.java
+++ Classes/Admin.java
@@ -1,7 +1,8 @@
 public class Admin extends User {
-
-public Admin (String first, String last, String ph) 
-{ super(first, last, ph); }
-
-public boolean isAdmin() { return true; }//Override
+    
+    //constuctor με ορίσματα όνομα, επώνυμο, τηλέφωνο
+    public Admin (String first, String last, String ph) 
+    { super(first, last, ph); }
+    
+    public boolean isAdmin() { return true; }//Override
 }

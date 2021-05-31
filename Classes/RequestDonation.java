abe5097fda119138491676943693ad942244bce8
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:21:04 +0300

    Update RequestDonation.java

--- Classes/RequestDonation.java
+++ Classes/RequestDonation.java
@@ -4,22 +4,26 @@ public class RequestDonation
     Entity entity;
     double quantity; 
     
+    //consuctor με ορίσματα entity και quantity
     public RequestDonation(Entity entity, double quantity){
     this.entity=entity;
     this.quantity=quantity;
     }
     
-    public int get_id(){return entity.getId();} //επιστρέφει το id για ένα requestdonation 
+    //επιστρέφει το id για ένα requestdonation 
+    public int get_id(){return entity.getId();} 
     
+    //τι κάνει;
     public Entity getEntity(){return entity;}
     
+    //setter-getter για το Quantity
     public void setQuantity(double quantity){this.quantity=quantity;} 
     public double getQuantity(){return quantity;}
         
-    //interface comparator
+    //interface comparator για την σύγκριση δύο αντικειμένων με βάση το ID
     private int compare(Entity d1, Entity d2)
         {
             if (d1.getId()==d2.getId()){return 0;} 
             else {return 1;}
         }
-    }
+}

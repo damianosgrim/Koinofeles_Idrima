a01315cf4b260fe61121cbff3ca9c3594ebb0196
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:16:31 +0300

    Update Beneficiary.java

--- Classes/Beneficiary.java
+++ Classes/Beneficiary.java
@@ -1,20 +1,22 @@
-import java.util.*;
+import java.util.*;  //wrappers
 public class Beneficiary extends User {
-int noPersons=1;
-
-//constructor χωρίς ορίσματα
-public Beneficiary(){}
-
-public Beneficiary(String firstname, String lastname, String phone, int noPersons) 
-{ super(firstname, lastname, phone);
-  this.noPersons=noPersons; }
-  
-public int getNoPersons() { return noPersons; }
-public void setNoPersons(int noPersons) {this.noPersons=noPersons;}
-
-// na koitaksoume wrappers
-
-ArrayList<RequestDonationList> receivedList = new ArrayList<RequestDonationList>();
-ArrayList<Request> requestsList = new ArrayList<Request>();
-
+    int noPersons=1; //αριθμός ατόμων της οικογένειας
+    
+    //constructor χωρίς ορίσματα
+    public Beneficiary(){}
+    
+    //constructor με ορίσματα όνομα, επώνυμο, τηλέφωνο και αριθμό ατόμων 
+    public Beneficiary(String firstname, String lastname, String phone, int noPersons) 
+    { super(firstname, lastname, phone);
+      this.noPersons=noPersons; }
+    
+    //getter-setter για τον αριθμό ατόμων
+    public int getNoPersons() { return noPersons; }
+    public void setNoPersons(int noPersons) {this.noPersons=noPersons;}
+    
+    //λίστα με τις παροχές που έχει λάβει ο Beneficiary
+    ArrayList<RequestDonationList> receivedList = new ArrayList<RequestDonationList>(); 
+    
+    //λίστα με τις παροχές που επιθυμεί να λάβει ο Beneficiary
+    ArrayList<Request> requestsList = new ArrayList<Request>();
 }

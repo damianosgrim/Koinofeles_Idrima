a330914396d9ef907333c6946917f91fc8b85d1d
mariak0 <83291320+mariak0@users.noreply.github.com>
2021-05-31 22:24:13 +0300

    Update User.java

--- Classes/User.java
+++ Classes/User.java
@@ -1,25 +1,29 @@
 public abstract class User {
-private  String firstname;  //Επιλέγουμε να έχουμε και όνομα και επώνυμο αντί για σκέτο name
-private  String lastname;
-private String phone;
-
-//constructor χωρίς ορίσματα
-public User(){}
-//με ορίσματα 
-public User (String first, String last, String ph) 
-{   firstname = first;
-    lastname = last;
-    phone = ph; }
-
-public  String getFirstName() { return firstname; }
-public  void setFirstName(String first) {firstname=first;}
-
-public  String getLastName() { return lastname; }
-public  void setLastName(String last) { lastname=last;}
-
-public String getPhone() { return phone; }
-public void setPhone(String phone) { this.phone=phone;}
-
-//Επιστρέφει false και για όλες τις υποκλάσεις εκτός απο το Admin που έχουμε κάνει override.
-public boolean isAdmin() { return false; } 
+    private  String firstname;  //Επιλέγουμε να έχουμε και όνομα και επώνυμο αντί για σκέτο name
+    private  String lastname;
+    private String phone;
+    
+    //constructor χωρίς ορίσματα
+    public User(){}
+    
+    //constructor με ορίσματα (προσθήκη πληροφοριών για χρήστη)
+    public User (String first, String last, String ph) 
+    {   firstname = first;
+        lastname = last;
+        phone = ph; }
+    
+    //getter-setter για το όνομα
+    public  String getFirstName() { return firstname; }
+    public  void setFirstName(String first) {firstname=first;}
+    
+    //getter-setter για επίθετο
+    public  String getLastName() { return lastname; }
+    public  void setLastName(String last) { lastname=last;}
+    
+    //getter-setter για τηλέφωνο
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone=phone;}
+    
+    //μέθοδος επιστρέφει false και για όλες τις υποκλάσεις εκτός από το Admin που έχουμε κάνει override.
+    public boolean isAdmin() { return false; } 
 }

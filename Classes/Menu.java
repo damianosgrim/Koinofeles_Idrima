2c4d59386915a617f55b2b8b3c7497c5bab8fac2
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-05-31 19:42:11 +0300

    Update Menu.java

--- Classes/Menu.java
+++ Classes/Menu.java
@@ -63,21 +63,26 @@ if(search==0) {
        ans=scan.nextLine(); }
     if ((ans.equals("n") || ans.equals("no") )) {ExitFromProgram();}
     else if(ans.equals("y") || ans.equals("yes") ) {
+        boolean exit=false;
+        do{
         System.out.println("Do you want to sign up as (1)Beneficiary or (2)Donator? (1/2)");
         ans=scan.nextLine();
         switch (ans) {
             case "1": 
             Menu.SignB();
+            exit=true;
             break;
             
             case "2": 
             Menu.SignD();
+            exit=true;
             break;
             
             default: 
                 System.out.println("Invalid command!");
             break;   
         }
+    }while(!exit);
 
     } 
 }
@@ -148,16 +153,21 @@ public static void MenuB(Beneficiary b) {
      if (havelogout==true) {
     System.out.println("Welcome " + b.getFirstName() + " " + b.getLastName() + ", your phone is " + b.getPhone() + ", you're benefiaciary and the number of your family members are: " + b.getNoPersons() + ".");
     havelogout=false; }
+    boolean exit=false;
+    do{
     System.out.println("1.Add Request  2.Show Requests  3.Commit  4.Back  5.Logout  6.Exit  (Enter the number of the option you want)");
     ans=scan.nextLine();
         switch (ans) {
-            case "1": 
+            case "1":
+            exit=true;
             break;
             
             case "2": 
+            exit=true;
             break;
             
-            case "3": 
+            case "3":
+            exit=true;
             break;
             
             case "4": 
@@ -166,21 +176,25 @@ public static void MenuB(Beneficiary b) {
               if ((ans.equals("n") || ans.equals("no") )) {LogOut();}
               else if(ans.equals("y") || ans.equals("yes") ) {
               Menu.MenuB(b);  
+              exit=true;
              }  
             break;
             
-            case "5": 
+            case "5":
             LogOut();
+            exit=true;
             break;
             
             case "6": 
             ExitFromProgram();
+            exit=true;
             break;
             
             default: 
             System.out.println("Invalid command!");
             break;   
         }
+    }while(!exit);
 }
 
 //Menu Donator
@@ -188,34 +202,45 @@ public static void MenuD(Donator d) {
     if (havelogout==true) {
     System.out.println("Welcome " + d.getFirstName() + " " + d.getLastName() + ", your phone is " + d.getPhone() + ", you're donator."); 
     havelogout=false; }
-    System.out.println("1.Add Offer  2.Show Offers  3.Commit  4.Back  5.Logout  6.Exit  (Enter the number of the option you want)");
-    ans=scan.nextLine();
+    boolean exit=false;
+    do{
+        System.out.println("1.Add Offer  2.Show Offers  3.Commit  4.Back  5.Logout  6.Exit  (Enter the number of the option you want)");
+        ans=scan.nextLine();
         switch (ans) {
             case "1":
+            boolean ex=false;
             System.out.println("Select what type of offer you want to add");
+            do{
             System.out.println("1.Matterial  2.Offer (Press b to back)");
             ans=scan.nextLine();
             switch (ans) {
                 case "1":
+                ex=true;
                 break;
                 
                 case "2":
+                ex=true;
                 break;
                 
                 case "b":
                 MenuD(d);
+                ex=true;
                 break;
                 
                 default: 
             System.out.println("Invalid command!");
             break;   
             }
+        }while(!ex);
+        exit=true;
             break;
             
             case "2": 
+            exit=true;
             break;
             
             case "3": 
+            exit=true;
             break;
             
             case "4": 
@@ -225,57 +250,103 @@ public static void MenuD(Donator d) {
               else if(ans.equals("y") || ans.equals("yes") ) {
               Menu.MenuD(d);  
              }  
+             exit=true;
             break;
             
             case "5": 
             LogOut();
+            exit=true;
             break;
             
             case "6": 
             ExitFromProgram();
+            exit=true;
             break;
             
             default: 
             System.out.println("Invalid command!");
             break;   
         }
+    }while(!exit);
 }
 
 //Menu Admin
-public static void MenuA(Admin a) {
+public static void MenuA(Admin a)  {
      if (havelogout==true) {
     System.out.println("Welcome " + a.getFirstName() + " " + a.getLastName() + ", your phone is " + a.getPhone() + ", you're admin."); 
     havelogout=false; }
+    
+    boolean exit=false;
+    do{
     System.out.println("1.View  2.Monitor Organization  3.Back  4.Logout  5.Exit  (Enter the number of the option you want)");
     ans=scan.nextLine();
+  
         switch (ans) {
             case "1": 
-            System.out.println("1.Material  2.Services" );
+             boolean Case=false;
+             boolean ex=false;
+             do{
+                System.out.println("1.Material  2.Services"  );
                 ans=scan.nextLine(); 
                 switch(ans){
                     case "1":
                                 Organization.showEntityinCategory(ans);
-                                System.out.println("Type the Name of the Material you want to check: ");
-                                ans=scan.nextLine();
-                                for (int i=0; i<Organization.entityList.size(); i++)
-                                {
-                                    Organization.entityList.get(i).getName();
+                               do{
+                                System.out.print("Type the name of a material for more details:");
+                               ans=scan.nextLine();
+                                boolean found=false;
+                                for(Entity i:Organization.getentityList()){
+                                    if(ans.equals(i.getName())){
+                                        for(int j=0;j<Organization.getentityList().size();j++){
+                                             if(Organization.getentityList().get(j).getName().equals(ans)){
+                                                System.out.println(Organization.getentityList().get(j). getEntityInfo());
+                                             }
+                                        }
+                                        found=true;
+                                        Case= true;
+                                    }
                                 }
-                                getEntityInfo();
-                    break;
+                                if(found==false){System.out.println("The material doesn't exist!");} 
+            
+                              }while(!Case);
+                         
+                    ex=true;
+                     
+                     break;
                 
                     case "2":
                                 Organization.showEntityinCategory(ans);
-                    break;
+                                do{
+                                System.out.print("Type the name of a service for more details:");
+                               ans=scan.nextLine();
+                                boolean found=false;
+                                for(Entity i:Organization.getentityList()){
+                                    if(ans.equals(i.getName())){
+                                        for(int j=0;j<Organization.getentityList().size();j++){
+                                             if(Organization.getentityList().get(j).getName().equals(ans)){
+                                                System.out.println(Organization.getentityList().get(j). getEntityInfo());
+                                             }
+                                        }
+                                        found=true;
+                                        Case= true;
+                                    }
+                                }
+                                if(found==false){System.out.println("The service doesn't exist!");} 
+            
+                              }while(!Case);  
+                    ex= true;    
+                     break;
                 
                     default: 
                                 System.out.println("Invalid command");
                     break;
                 }
-            break;
+            }while(!ex);
+            exit=true;
+              break;
             
             case "2": 
-                        
+            exit=true;  
             break;
             
             case "3": 
@@ -285,20 +356,24 @@ public static void MenuA(Admin a) {
               else if(ans.equals("y") || ans.equals("yes") ) {
               Menu.MenuA(a);  
              }  
+             exit=true;
             break;
             
             case "4": 
             LogOut();
+            exit=true;
             break;
             
             case "5": 
             ExitFromProgram();
+            exit=true;
             break;
             
             default: 
             System.out.println("Invalid command!");
             break;   
         }
+    }while(!exit);
 }
 
 //Αποσύνδεση χρήστη και επιστοροφή στην αρχική σελίδα

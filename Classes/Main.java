baadb775c3dc8e8b153b8a4c473c06433f287f73
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2021-06-01 14:24:42 +0300

    Update Main.java

--- Classes/Main.java
+++ Classes/Main.java
@@ -4,30 +4,44 @@ public static void main(String[] args) {
 Admin ad = new Admin("Chryssa", "Pateli", "13033");
 Organization org = new Organization("KoinofelesIdrimaCEID", ad);
 
+
 Menu menu = new Menu();
 
 Material mlk = new Material();
 mlk.setName("Milk");
+mlk.setDescription("Dairy product 1lt ");
+mlk.getId();
 org.addEntity(mlk);
+
 
 Material sug = new Material();
 sug.setName("Sugar");
+sug.setDescription("Sweetener 1kg ");
+sug.getId(); 
 org.addEntity(sug);
 
 Material rc = new Material();
 rc.setName("Rice");
+rc.setDescription("Cereal grain 500gr ");
+rc.getId();
 org.addEntity(rc);
 
 Service mds = new Service();
 mds.setName("MedicalSupport");
+mds.setDescription("Health service");
+mds.getId();
 org.addEntity(mds);
 
 Service nrs = new Service();
 nrs.setName("NurserySupport");
+nrs.setDescription("Health service");
+nrs.getId();
 org.addEntity(nrs);
 
 Service bs = new Service();
 bs.setName("BabySitting");
+bs.setDescription("Housekeeping ");
+bs.getId();
 org.addEntity(bs);
 
 
@@ -36,8 +50,8 @@ Beneficiary b2 = new Beneficiary("Damianos", "Grimanis", "13031", 2);  //Paroxes
 Donator d = new Donator("Eleni", "Vogiatzaki", "13030");
 org.insertBeneficiary(b1);
 org.insertBeneficiary(b2);
-org.insertDonator(d); 
-
+org.insertDonator(d);
+ 
 try
 {
     menu.Scan();
@@ -48,3 +62,4 @@ catch (NullLineReadedException nlre)
 }
 }
 }
+
